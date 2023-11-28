8b802d0064a63e0e6452313b9a0cef4a27b176a7
Martins Jaunzems <martins.jaunzems48@gmail.com>
2023-11-28 16:01:21 +0200

    Initial commit

---
+++ src/test/java/com/example/HealthyDog/HealthyDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthyDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
