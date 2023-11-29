e9298cfdb21396f3018610bd450f2a9c836df69e
TatjanaVlasova <vlasovaphotography1@gmail.com>
2023-11-29 14:03:32 +0200

    JPA Buddy

--- src/main/java/com/example/HealthyDog/HealthyDogApplication.java
+++ src/main/java/com/example/HealthyDog/HealthyDogApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthyDogApplication {
+public class
+HealthyDogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthyDogApplication.class, args);
