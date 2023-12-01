fa219b66fd161b0b52620cac3dab3bc36154b840
Martins Jaunzems <martins.jaunzems48@gmail.com>
2023-12-01 15:00:18 +0200

    Form get and post redirect

--- src/main/java/com/example/HealthyDog/Controllers/FormController.java
+++ src/main/java/com/example/HealthyDog/Controllers/FormController.java
@@ -19,16 +19,16 @@ public class FormController {
     @Autowired
     private AllergicFoodsRepository allergicFoodsRepository;
 
-    @GetMapping("/form")
-    public ModelAndView form(){
+    @PostMapping("/processForm")
+    public ModelAndView form(Model model){
+        Iterable<AllergicFoodsEntity> options = allergicFoodsRepository.findAll();
+        model.addAttribute("options", options);
         return new ModelAndView("form");
     }
 
-    @PostMapping("/processForm")
+    @GetMapping("/form")
     public ModelAndView form(@RequestParam("type") String type, HttpSession session, Model model){
-        Iterable<AllergicFoodsEntity> options = allergicFoodsRepository.findAll();
-        model.addAttribute("options", options);
         session.setAttribute("type", type);
-        return new ModelAndView("form");
+        return new ModelAndView("redirect:/form");
     }
 }
