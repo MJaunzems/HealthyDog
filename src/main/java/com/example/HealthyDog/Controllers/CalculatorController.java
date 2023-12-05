7a5f574515f0a5beec4920a4487e58738e0a6100
Jana Shv <janashvets@gmail.com>
2023-12-05 11:48:31 +0200

    exceptions in calculator

--- src/main/java/com/example/HealthyDog/Controllers/CalculatorController.java
+++ src/main/java/com/example/HealthyDog/Controllers/CalculatorController.java
@@ -2,6 +2,8 @@ package com.example.HealthyDog.Controllers;
 
 import com.example.HealthyDog.CalculationRequest;
 import com.example.HealthyDog.Services.CalculatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,19 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorController.class);
+
     @Autowired
     private CalculatorService calculatorService;
 
     @PostMapping("/calculate")
     public String calculate(@RequestBody CalculationRequest request) {
-        double rer = calculatorService.calculateRER(request.getWeight());
-        double k = calculatorService.validateAndSetK(request.getActivityLevel());
-        double dailyCalories = calculatorService.calculateDailyCalories(rer, k);
-        double dryFoodGrams = calculatorService.calculateDryFoodGrams(dailyCalories, request.getDryFoodCalorieContent());
-        double cannedFoodGrams = calculatorService.calculateCannedFoodGrams(dailyCalories, request.getCannedFoodCalorieContent());
+        try {
+            double rer = calculatorService.calculateRER(request.getWeight());
+            double k = calculatorService.validateAndSetK(request.getActivityLevel());
+            double dailyCalories = calculatorService.calculateDailyCalories(rer, k);
+            double dryFoodGrams = calculatorService.calculateDryFoodGrams(dailyCalories, request.getDryFoodCalorieContent());
+            double cannedFoodGrams = calculatorService.calculateCannedFoodGrams(dailyCalories, request.getCannedFoodCalorieContent());
 
-        return String.format("RER: %.2f, Daily Calories: %.2f, Dry Food Grams: %.2f, Canned Food Grams: %.2f",
-                rer, dailyCalories, dryFoodGrams, cannedFoodGrams);
+            return String.format("RER: %.2f, Daily Calories: %.2f, Dry Food Grams: %.2f, Canned Food Grams: %.2f",
+                    rer, dailyCalories, dryFoodGrams, cannedFoodGrams);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Invalid input: {}", e.getMessage());
+            return "Invalid input. Please check your request.";
+        } catch (Exception e) {
+            LOGGER.error("An unexpected error occurred: {}", e.getMessage());
+            return "An unexpected error occurred. Please try again later.";
+        }
     }
-
 }
+
