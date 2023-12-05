23d96147c7155dac96673a9c5021c0a8fefb9095
Jana Shv <janashvets@gmail.com>
2023-12-05 12:54:35 +0200

    exceptions in calculator

--- src/main/java/com/example/HealthyDog/Controllers/CannedFoodController.java
+++ src/main/java/com/example/HealthyDog/Controllers/CannedFoodController.java
@@ -2,6 +2,8 @@ package com.example.HealthyDog.Controllers;
 
 import com.example.HealthyDog.Entities.CannedFoodEntity;
 import com.example.HealthyDog.Services.CannedFoodService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import java.util.List;
 public class CannedFoodController {
 
     private final CannedFoodService cannedFoodService;
+    private static final Logger logger = LoggerFactory.getLogger(CannedFoodController.class);
 
     @Autowired
     public CannedFoodController(CannedFoodService cannedFoodService) {
@@ -20,7 +23,11 @@ public class CannedFoodController {
 
     @GetMapping("/findByCannedCompany/{cannedCompany}")
     public List<CannedFoodEntity> findByCannedCompany(@PathVariable String cannedCompany) {
-        return cannedFoodService.findByCannedCompany(cannedCompany);
+        try {
+            return cannedFoodService.findByCannedCompany(cannedCompany);
+        } catch (Exception e) {
+            logger.error("Error occurred while processing request for canned foods by company: {}", cannedCompany, e);
+            return List.of();
+        }
     }
-
 }
--- src/main/java/com/example/HealthyDog/Services/CannedFoodService.java
+++ src/main/java/com/example/HealthyDog/Services/CannedFoodService.java
@@ -2,6 +2,8 @@ package com.example.HealthyDog.Services;
 
 import com.example.HealthyDog.Entities.CannedFoodEntity;
 import com.example.HealthyDog.Repositories.CannedFoodRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.List;
 public class CannedFoodService {
 
     private final CannedFoodRepository cannedFoodRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CannedFoodService.class);
 
     @Autowired
     public CannedFoodService(CannedFoodRepository cannedFoodRepository) {
@@ -18,7 +21,11 @@ public class CannedFoodService {
     }
 
     public List<CannedFoodEntity> findByCannedCompany(String cannedCompany) {
-        return cannedFoodRepository.findByCannedCompany(cannedCompany);
+        try {
+            return cannedFoodRepository.findByCannedCompany(cannedCompany);
+        } catch (Exception e) {
+            logger.error("Error occurred while fetching canned foods by company: {}", cannedCompany, e);
+            throw new RuntimeException("Error occurred while fetching canned foods by company: " + cannedCompany, e);
+        }
     }
-
 }
