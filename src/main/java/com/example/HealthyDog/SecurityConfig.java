0e0f3f1aba7fb24d379e021b8fbb2e143abdc987
Martins Jaunzems <martins.jaunzems48@gmail.com>
2023-12-05 13:55:57 +0200

    Login and security setup with h2 console

--- src/main/java/com/example/HealthyDog/SecurityConfig.java
+++ src/main/java/com/example/HealthyDog/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +17,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
@@ -29,16 +32,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(toH2Console())
+                        .disable())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/foodCalc", "/processType", "/admin/**", "/h2-console/**").permitAll()
-                        //.requestMatchers().hasAuthority("ADMIN")
+                        .requestMatchers("/", "/foodCalc", "/processType").permitAll()
+                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll().invalidateHttpSession(true).clearAuthentication(true));
+                .logout((logout) -> logout.permitAll().invalidateHttpSession(true).clearAuthentication(true))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return http.build();
     }
     @Autowired
