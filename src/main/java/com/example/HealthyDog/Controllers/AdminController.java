9376ca7e2e7dca8a08de7f2014b9dc4905c88a1c
Martins Jaunzems <martins.jaunzems48@gmail.com>
2023-12-07 16:26:14 +0200

    Login redirect, admin page delete, logout, full food list

--- src/main/java/com/example/HealthyDog/Controllers/AdminController.java
+++ src/main/java/com/example/HealthyDog/Controllers/AdminController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
@@ -18,11 +21,19 @@ public class AdminController {
     @GetMapping("/admin")
     public String showAdmin(Model model){
         model.addAttribute("foodEntity", new FoodEntity());
+        List<FoodEntity> foods = foodRepository.findAll();
+        model.addAttribute("foods", foods);
         return "admin";
     }
     @PostMapping("/admin/add-food")
     public String addFood(@ModelAttribute FoodEntity foodEntity){
         foodRepository.save(foodEntity);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/delete-food")
+    public String deleteFood(@RequestParam long id){
+        foodRepository.deleteById(id);
         return "redirect:/admin";
     }
 }
