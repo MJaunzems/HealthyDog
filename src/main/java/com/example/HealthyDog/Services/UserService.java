ad784a9f9f79cfa8d9d751e3f040f57c183991ce
VlasovaTatjana <150793221+VlasovaTatjana@users.noreply.github.com>
2023-12-07 13:20:06 +0200

    Registration impl

--- src/main/java/com/example/HealthyDog/Services/UserService.java
+++ src/main/java/com/example/HealthyDog/Services/UserService.java
@@ -5,24 +5,27 @@ import com.example.HealthyDog.MyUserDetails;
 import com.example.HealthyDog.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.context.annotation.Role;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
-public class UserService implements UserDetailsService{
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
     @Lazy
     @Autowired
     PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
@@ -51,14 +54,33 @@ public class UserService implements UserDetailsService{
     public void updateUser(UserDetails userDetails, String username, int phone, String email) {
         String currentUsername = userDetails.getUsername();
         Optional<UserEntity> userDetailsOptional = userRepository.findByUserNickname(currentUsername);
-        if(userDetailsOptional.isPresent()){
+        if (userDetailsOptional.isPresent()) {
             UserEntity userEntity = userDetailsOptional.get();
             userEntity.setUserNickname(username);
             userEntity.setUserPhone(phone);
             userEntity.setUserEmail(email);
             userRepository.save(userEntity);
-        }else{
+        } else {
             throw new UsernameNotFoundException("User not found with username: " + currentUsername);
         }
     }
+
+    public void registerUser(String username, String email, int userPhone, String password) {
+        if (userRepository.findByUserNickname(username).isPresent()) {
+            throw new IllegalArgumentException("Username already exists: " + username);
+        }
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        UserEntity newUser = new UserEntity();
+        newUser.setUserNickname(username);
+        newUser.setUserEmail(email);
+        newUser.setUserPhone(userPhone);
+        newUser.setUserPassword(passwordEncoder.encode(password));
+        newUser.setRoles("USER");
+        userRepository.save(newUser);
+    }
+
 }
+
+
