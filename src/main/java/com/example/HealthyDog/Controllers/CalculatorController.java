b30d2e777c52ced14ebe9f571a1c9eb23e9ae7c5
Martins Jaunzems <martins.jaunzems48@gmail.com>
2023-12-08 11:58:01 +0200

    Print error handling

--- src/main/java/com/example/HealthyDog/Controllers/CalculatorController.java
+++ src/main/java/com/example/HealthyDog/Controllers/CalculatorController.java
@@ -17,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,18 +72,19 @@ public class CalculatorController {
     }
 
     @GetMapping("/generate-pdf")
-    public String generatePdfReport(Model model) {
+    public ModelAndView generatePdfReport(Model model, RedirectAttributes redirectAttributes) {
 
         if (!MENU.isEmpty()) {
             try {
                 pdfService.generatePdf(MENU);
-                model.addAttribute("message", "PDF report generated successfully!");
+                redirectAttributes.addFlashAttribute("message", "PDF report generated successfully!");
             } catch (Exception e) {
-                model.addAttribute("error", "Error generating PDF report: " + e.getMessage());
+                redirectAttributes.addFlashAttribute("errorPrint", "Error generating PDF report: " + e.getMessage());
             }
-            return "dryfoods";
+            return new ModelAndView("redirect:/dryfoods");
         } else {
-            return "error";
+            redirectAttributes.addFlashAttribute("errorPrint", "You must complete the form.");
+            return new ModelAndView("redirect:/dryfoods");
         }
     }
 
