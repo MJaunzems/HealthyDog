cec5a86f7fa298efe563e481dc5638372f272019
Jana Shv <janashvets@gmail.com>
2023-12-09 22:20:54 +0200

    design again

--- src/main/java/com/example/HealthyDog/HealthyDogApplication.java
+++ src/main/java/com/example/HealthyDog/HealthyDogApplication.java
@@ -1,6 +1,6 @@
 package com.example.HealthyDog;
 
-import com.example.HealthyDog.Repositories.UserRepository;
+import  com.example.HealthyDog.Repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
